971ec3c38fefa30fb1ff8d2942d481dcb6147e9a
cwoodcox-work <cpwoodco@gmail.com>
2025-10-01 18:39:58 -0500

    Added logic to allow negative numbers.

--- src/CalcLogic.java
+++ src/CalcLogic.java
@@ -14,59 +14,66 @@ public class CalcLogic {
 //need to work on consecutive multiplication/division operations.
     public String result(String text){
 
-        ArrayList<String> splitEquate = new ArrayList<>(asList(text.split("(?<=[+\\-/*])|(?=[+\\-/*])")));
+        ArrayList<String> splitEquate = new ArrayList<>(asList(text.split("(?<=[+/*])|(?=[+\\-/*])")));
         String[] tempHold;
         int tempNum1 = 0;
         int tempNum2 = 0;
         String holdSign = "";
+        boolean prevNum = false;
+        boolean curNum = true;
+        boolean negNum = false;
         int total = 0;
         int loopCount = 0;
         while(true) {
             loopCount += 1;
             for (int i = 0; i < splitEquate.size(); i++) {
-                if (i % 2 == 0) {
-                    try {
-                        if (holdSign.equals("")) {
-                            tempNum1 = Integer.valueOf(splitEquate.get(i));
-                        } else if (holdSign.equals("/")) {
-                            tempNum2 = tempNum1 / Integer.valueOf(splitEquate.get(i));
-                            splitEquate.subList(i - 2, i + 1).clear();
-                            splitEquate.add(i - 2, String.valueOf(tempNum2));
-                            tempNum1 = Integer.valueOf(tempNum2);
-                            i -= 2;
-                        } else if (holdSign.equals("*")) {
-                            tempNum2 = tempNum1 * Integer.valueOf(splitEquate.get(i));
-                            splitEquate.subList(i - 2, i + 1).clear();
-                            splitEquate.add(i - 2, String.valueOf(tempNum2));
-                            tempNum1 = Integer.valueOf(tempNum2);
-                            i -= 2;
-                        } else if (holdSign.equals("+")) {
-                            if (loopCount == 1) {
-                                tempNum1 = Integer.valueOf(splitEquate.get(i));
-                            } else {
-                                tempNum2 = tempNum1 + Integer.valueOf(splitEquate.get(i));
-                                splitEquate.subList(i - 2, i + 1).clear();
-                                splitEquate.add(i - 2, String.valueOf(tempNum2));
-                                tempNum1 = Integer.valueOf(tempNum2);
-                                i -= 2;
-                            }
-                        } else if (holdSign.equals("-")) {
-                            if (loopCount == 1) {
-                                tempNum1 = Integer.valueOf(splitEquate.get(i));
-                            } else {
-                                tempNum2 = tempNum1 - Integer.valueOf(splitEquate.get(i));
-                                splitEquate.subList(i - 2, i + 1).clear();
-                                splitEquate.add(i - 2, String.valueOf(tempNum2));
-                                tempNum1 = Integer.valueOf(tempNum2);
-                                i -= 2;
-                            }
-                        }
-                    }catch(NumberFormatException e){
-                        String problem = "BAD INPUT";
-                        return problem;
+                try{
+                    Integer.valueOf(splitEquate.get(i));
+                    curNum = true;
+                }catch(NumberFormatException e){
+                    curNum = false;
+                }
+                if (curNum){
+                    if(prevNum && Integer.valueOf(splitEquate.get(i))<0){
+                        negNum = true;
+                        holdSign = "+";
                     }
-                } else {
+                    if (holdSign.equals("")) {
+                        tempNum1 = Integer.valueOf(splitEquate.get(i));
+                    } else if (holdSign.equals("/")) {
+                        tempNum2 = tempNum1 / Integer.valueOf(splitEquate.get(i));
+                        splitEquate.subList(i - 2, i + 1).clear();
+                        splitEquate.add(i - 2, String.valueOf(tempNum2));
+                        tempNum1 = Integer.valueOf(tempNum2);
+                        i -= 2;
+                    } else if (holdSign.equals("*")) {
+                        tempNum2 = tempNum1 * Integer.valueOf(splitEquate.get(i));
+                        splitEquate.subList(i - 2, i + 1).clear();
+                        splitEquate.add(i - 2, String.valueOf(tempNum2));
+                        tempNum1 = Integer.valueOf(tempNum2);
+                        i -= 2;
+                    } else if (holdSign.equals("+")) {
+                        if (loopCount == 1 && negNum) {
+                            splitEquate.add(i, String.valueOf(holdSign));
+                            i++;
+                            tempNum1 = Integer.valueOf(splitEquate.get(i));
+                        }else if(loopCount == 1){
+                            tempNum1 = Integer.valueOf(splitEquate.get(i));
+                        }else {
+                            tempNum2 = tempNum1 + Integer.valueOf(splitEquate.get(i));
+                            splitEquate.subList(i - 2, i + 1).clear();
+                            splitEquate.add(i - 2, String.valueOf(tempNum2));
+                            tempNum1 = Integer.valueOf(tempNum2);
+                            i -= 2;
+                        }
+                    }
+                    prevNum = true;
+                } else if(prevNum){
                     holdSign = splitEquate.get(i);
+                    prevNum = false;
+                }else{
+                    String problem = "Consecutive Operators";
+                    return problem;
                 }
             }
             if(splitEquate.size() == 1){
